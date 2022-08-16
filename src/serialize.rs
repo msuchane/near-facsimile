0997849489f63c5fdb37ede0a0cbe6acd36bbe69
Marek Suchánek <msuchane@redhat.com>
2022-08-16 18:23:52 +0200

    Separate CSV from the general serialization

--- src/serialize.rs
+++ src/serialize.rs
@@ -5,25 +5,32 @@ use color_eyre::Result;
 use crate::Cli;
 use crate::Comparison;
 
-/// Serialize the resulting comparisons as a CSV table.
+/// Serialize the resulting comparisons as a structured file.
 pub fn serialize(mut comparisons: Vec<Comparison>, options: &Cli) -> Result<()> {
+    // Sort from highest to lowest. You can't sort f64 values, so convert them to u32
+    // with a precision of percentage with a single decimal place, then subtract from 1000.
+    comparisons
+        .sort_by_key(|comparison| 1000 - (comparison.similarity_pct.0 * 10.0).round() as i32);
+
+    as_csv(&comparisons, options)?;
+
+    Ok(())
+}
+
+/// Serialize and save the comparisons as a CSV file.
+fn as_csv(comparisons: &[Comparison], options: &Cli) -> Result<()> {
     // Prepare to write to the CSV file.
     let mut wtr = csv::Writer::from_path(&options.csv)?;
 
     // The CSV header:
     wtr.write_record(&["% similar", "File 1", "File 2"])?;
 
-    // Sort from highest to lowest. You can't sort f64 values, so convert them to u32
-    // with a precision of percentage with a single decimal place, then subtract from 1000.
-    comparisons
-        .sort_by_key(|comparison| 1000 - (comparison.similarity_pct.0 * 10.0).round() as i32);
-
     // Each comparison entry writes a row in the CSV table.
-    for comparison in &comparisons {
+    for comparison in comparisons {
         wtr.write_record(&[
             format!("{:.1}", comparison.similarity_pct.0),
-            stripped_path(&comparison.path1, options)?,
-            stripped_path(&comparison.path2, options)?,
+            stripped_path(comparison.path1, options)?,
+            stripped_path(comparison.path2, options)?,
         ])?;
     }
 
