f93977327e1d0733e2666ba8dbefc045176eb29b
Marek Suchánek <msuchane@redhat.com>
2023-01-26 18:28:41 +0100

    Generate the man page in the standardized directory

--- build.rs
+++ build.rs
@@ -14,13 +14,16 @@ mod cli;
 use cli::Cli;
 
 fn main() -> std::io::Result<()> {
+    let out_dir = std::path::PathBuf::from(std::env::var_os("OUT_DIR")
+        .ok_or_else(|| std::io::ErrorKind::NotFound)?);
+
     let cmd: clap::Command = Cli::command();
 
     let man = clap_mangen::Man::new(cmd);
     let mut buffer: Vec<u8> = Default::default();
     man.render(&mut buffer)?;
 
-    std::fs::write("near-facsimile.1", buffer)?;
+    std::fs::write(out_dir.join("near-facsimile.1"), buffer)?;
 
     Ok(())
 }
